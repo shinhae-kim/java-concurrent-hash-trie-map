901846ad788819ece80fc4a740785e920ef98057
shinhae-kim <sk3364@cornell.edu>
2025-01-09 02:51:33 +0000

    more checkpoints

--- src/test/java/com/romix/scala/collection/concurrent/TestMultiThreadMapIterator.java
+++ src/test/java/com/romix/scala/collection/concurrent/TestMultiThreadMapIterator.java
@@ -33,8 +33,10 @@ public class TestMultiThreadMapIterator {
       		System.out.println ("Checkpoint - 1");  
                 final int threadNo = i;
                 es.execute (new Runnable () {
+      		    System.out.println ("Checkpoint - 1(1)");  
                     @Override
                     public void run () {
+      		        System.out.println ("Checkpoint - 1(2)");  
                         for (final Iterator<Map.Entry<Object, Object>> i = bt.entrySet ().iterator (); i.hasNext ();) {
       			    System.out.println ("Checkpoint - 2");  
                             final Entry<Object, Object> e = i.next ();
